f7dcd00d6a6b11a1dcf290bd3e7d741e67edd9e8
Adam Abdulkadir <78934570+aabdulk862@users.noreply.github.com>
2024-01-19 10:02:52 -0500

    loading screen

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -2,20 +2,12 @@ import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import MediaCard from "./MediaCard";
-import { Button, Typography } from "@mui/material";
+import { Button, Typography  } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Menu = () => {
-  // const [menuItems, setMenuItems] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(`${process.env.REACT_APP_API_URL}/menuItems`)
-  //     .then((response) => response.json())
-  //     .then((data) => setMenuItems(data))
-  //     .catch((error) => console.log(error));
-  // }, [menuItems]);
-
   const [menuItems, setMenuItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchMenuItems = async () => {
     try {
@@ -24,8 +16,10 @@ const Menu = () => {
       );
       const data = await response.json();
       setMenuItems(data);
+      setLoading(false); // Set loading to false after fetching data
     } catch (error) {
       console.error(error);
+      setLoading(false); // Set loading to false in case of an error
     }
   };
 
@@ -86,190 +80,205 @@ const Menu = () => {
 
   return (
     <Container maxWidth="xl" sx={{ paddingBottom: "7rem" }}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: "2rem",
-          paddingBottom: "3rem",
-        }}
-      >
+      {loading ? (
         <Box
           sx={{
-            paddingTop: "1rem",
             display: "flex",
-            flexWrap: "wrap",
+            alignItems: "center",
             justifyContent: "center",
+            height: "100vh",
+          }}
+          aria-describedby="loading-region"
+          aria-busy={loading}
+        >
+          <Typography variant="h4">Please wait while menu items are loading...</Typography>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "2rem",
+            paddingBottom: "3rem",
           }}
         >
-          <Button
-            variant="contained"
-            size="large"
+          <Box
             sx={{
-              fontSize: "1rem",
-              bgcolor: "#4CAF50",
-              "&:hover": {
-                bgcolor: "#45a049",
-              },
-              margin: ".5rem",
-              width: "70%",
+              paddingTop: "1rem",
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
             }}
-            component={Link}
-            to="/add"
           >
-            Add Menu Item
-          </Button>
-          <Button
-            variant="contained"
-            size="large"
+            <Button
+              variant="contained"
+              size="large"
+              sx={{
+                fontSize: "1rem",
+                bgcolor: "#4CAF50",
+                "&:hover": {
+                  bgcolor: "#45a049",
+                },
+                margin: ".5rem",
+                width: "70%",
+              }}
+              component={Link}
+              to="/add"
+            >
+              Add Menu Item
+            </Button>
+            <Button
+              variant="contained"
+              size="large"
+              sx={{
+                fontSize: "1rem",
+                bgcolor: "#F30A49",
+                "&:hover": {
+                  bgcolor: "#FA99B3",
+                },
+                margin: ".5rem",
+                width: "70%",
+              }}
+              onClick={handleReset}
+            >
+              Reset Menu Items
+            </Button>
+          </Box>
+          <Typography
+            variant="h1"
             sx={{
-              fontSize: "1rem",
-              bgcolor: "#F30A49",
-              "&:hover": {
-                bgcolor: "#FA99B3",
-              },
-              margin: ".5rem",
-              width: "70%",
+              fontWeight: 700,
+              textDecoration: "underline",
+              color: "#F30A49",
+              fontSize: { xs: "2em", md: "2rem", lg: "2rem" },
+              padding: "1rem",
+              textAlign: "center",
             }}
-            onClick={handleReset}
           >
-            Reset Menu Items
-          </Button>
+            Appetizers
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "1rem",
+            }}
+          >
+            {appetizers.map((item) => (
+              <MediaCard
+                id={item.id}
+                key={item.id}
+                name={item.name}
+                img={item.img}
+                description={item.description}
+                onDelete={handleDelete}
+                onUpdate={(updatedItem) => handleUpdate(item.id, updatedItem)}
+              />
+            ))}
+          </Box>
+          <Typography
+            variant="h1"
+            sx={{
+              fontWeight: 700,
+              textDecoration: "underline",
+              color: "#F30A49",
+              fontSize: { xs: "2em", md: "2rem", lg: "2rem" },
+              padding: "1rem",
+              textAlign: "center",
+            }}
+          >
+            Entrees
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "1rem",
+            }}
+          >
+            {entrees.map((item) => (
+              <MediaCard
+                id={item.id}
+                key={item.id}
+                name={item.name}
+                img={item.img}
+                description={item.description}
+                onDelete={handleDelete}
+                onUpdate={(updatedItem) => handleUpdate(item.id, updatedItem)}
+              />
+            ))}
+          </Box>
+          <Typography
+            variant="h1"
+            sx={{
+              fontWeight: 700,
+              textDecoration: "underline",
+              color: "#F30A49",
+              fontSize: { xs: "2em", md: "2rem", lg: "2rem" },
+              padding: "1rem",
+              textAlign: "center",
+            }}
+          >
+            Desserts
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "1rem",
+            }}
+          >
+            {desserts.map((item) => (
+              <MediaCard
+                id={item.id}
+                key={item.id}
+                name={item.name}
+                img={item.img}
+                description={item.description}
+                onDelete={handleDelete}
+                onUpdate={(updatedItem) => handleUpdate(item.id, updatedItem)}
+              />
+            ))}
+          </Box>
+          <Typography
+            variant="h1"
+            sx={{
+              fontWeight: 700,
+              textDecoration: "underline",
+              color: "#F30A49",
+              fontSize: { xs: "2em", md: "2rem", lg: "2rem" },
+              padding: "1rem",
+              textAlign: "center",
+            }}
+          >
+            Beverages
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "1rem",
+            }}
+          >
+            {beverages.map((item) => (
+              <MediaCard
+                id={item.id}
+                key={item.id}
+                name={item.name}
+                img={item.img}
+                description={item.description}
+                onDelete={handleDelete}
+                onUpdate={(updatedItem) => handleUpdate(item.id, updatedItem)}
+              />
+            ))}
+          </Box>
         </Box>
-        <Typography
-          variant="h1"
-          sx={{
-            fontWeight: 700,
-            textDecoration: "underline",
-            color: "#F30A49",
-            fontSize: { xs: "2em", md: "2rem", lg: "2rem" },
-            padding: "1rem",
-            textAlign: "center",
-          }}
-        >
-          Appetizers
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            gap: "1rem",
-          }}
-        >
-          {appetizers.map((item) => (
-            <MediaCard
-              id={item.id}
-              key={item.id}
-              name={item.name}
-              img={item.img}
-              description={item.description}
-              onDelete={handleDelete}
-              onUpdate={(updatedItem) => handleUpdate(item.id, updatedItem)}
-            />
-          ))}
-        </Box>
-        <Typography
-          variant="h1"
-          sx={{
-            fontWeight: 700,
-            textDecoration: "underline",
-            color: "#F30A49",
-            fontSize: { xs: "2em", md: "2rem", lg: "2rem" },
-            padding: "1rem",
-            textAlign: "center",
-          }}
-        >
-          Entrees
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            gap: "1rem",
-          }}
-        >
-          {entrees.map((item) => (
-            <MediaCard
-              id={item.id}
-              key={item.id}
-              name={item.name}
-              img={item.img}
-              description={item.description}
-              onDelete={handleDelete}
-              onUpdate={(updatedItem) => handleUpdate(item.id, updatedItem)}
-            />
-          ))}
-        </Box>
-        <Typography
-          variant="h1"
-          sx={{
-            fontWeight: 700,
-            textDecoration: "underline",
-            color: "#F30A49",
-            fontSize: { xs: "2em", md: "2rem", lg: "2rem" },
-            padding: "1rem",
-            textAlign: "center",
-          }}
-        >
-          Desserts
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            gap: "1rem",
-          }}
-        >
-          {desserts.map((item) => (
-            <MediaCard
-              id={item.id}
-              key={item.id}
-              name={item.name}
-              img={item.img}
-              description={item.description}
-              onDelete={handleDelete}
-              onUpdate={(updatedItem) => handleUpdate(item.id, updatedItem)}
-            />
-          ))}
-        </Box>
-        <Typography
-          variant="h1"
-          sx={{
-            fontWeight: 700,
-            textDecoration: "underline",
-            color: "#F30A49",
-            fontSize: { xs: "2em", md: "2rem", lg: "2rem" },
-            padding: "1rem",
-            textAlign: "center",
-          }}
-        >
-          Beverages
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            gap: "1rem",
-          }}
-        >
-          {beverages.map((item) => (
-            <MediaCard
-              id={item.id}
-              key={item.id}
-              name={item.name}
-              img={item.img}
-              description={item.description}
-              onDelete={handleDelete}
-              onUpdate={(updatedItem) => handleUpdate(item.id, updatedItem)}
-            />
-          ))}
-        </Box>
-      </Box>
+      )}
     </Container>
   );
 };
